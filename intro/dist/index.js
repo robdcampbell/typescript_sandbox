768493c35180c3bec8184eb76ca20d7ad567265a
robdcampbell <rdeancampbell@gmail.com>
2022-08-11 10:58:24 -0700

    Continuing tsconfig edits and setup

--- intro/dist/index.js
+++ intro/dist/index.js
@@ -4,6 +4,9 @@
 // SAVE LOCALLY TO PROJ: npm install typescript --save-dev
 // SETUP:
 //  tsc --init (sets up ts_config file)
+//  "module": "ES6"
+//  "rootDir": "./src" /* Specify the root folder within your source files. */,
+//  "outDir": "./dist" /* Specify an output folder for all emitted files. */,
 // COMPILE: tsc fileName or tsc --watch fileName
-let id = 5;
-console.log(typeof id);
+let id = 9;
+console.log(id);
--- intro/src/index.ts
+++ intro/src/index.ts
@@ -5,9 +5,12 @@
 
 // SETUP:
 //  tsc --init (sets up ts_config file)
+//  "module": "ES6"
+//  "rootDir": "./src" /* Specify the root folder within your source files. */,
+//  "outDir": "./dist" /* Specify an output folder for all emitted files. */,
 
 // COMPILE: tsc fileName or tsc --watch fileName
 
-let id: number = 5;
+let id: number = 9;
 
-console.log(typeof id);
+console.log(id);
