bd1654a3e5ee7ba6cced904c479887de71a3d35a
robdcampbell <rdeancampbell@gmail.com>
2022-08-23 23:36:39 -0400

    Continuing TS cardio

--- intro/dist/index.js
+++ intro/dist/index.js
@@ -8,5 +8,59 @@
 //  "rootDir": "./src" /* Specify the root folder within your source files. */,
 //  "outDir": "./dist" /* Specify an output folder for all emitted files. */,
 // COMPILE: tsc fileName or tsc --watch fileName
+//////// //////// //////// ////////
+// BASIC TYPES
 let id = 9;
-console.log(id);
+let company = "Omnicorp";
+let isPublihed = true;
+let x = "Hello";
+x = false;
+let ids = [1, 2, 3, 4, 5]; // notation to signify an array of numbers
+// ids.push('Hello') // Would throw an error.
+let arr = [1, "two", true];
+//////// //////// //////// ////////
+// Tuple
+let person = [1, "Rob", true]; // datatype must match index position
+//////// //////// //////// ////////
+// Tuple Array
+let employee;
+employee = [
+    [1, "Rob"],
+    [2, "Burt"],
+    [3, "Gordie"],
+];
+// Unions - when you need a variable to hold more than one dataType
+let ex3 = 22;
+// enum - define a set up named constants (numeric or string)
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up, Direction1.Right); // should log 0, 3
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Left); // Retuns "Left"
+const user = {
+    id: 1,
+    name: "John",
+};
+// Type Assertion - when you want to treat a specific entity as a different type
+let cid = 1;
+let customerId = cid;
+// customerId = true; // Will throw an error, because customerId needs to be a number;
+let customerId2 = cid;
+//////// //////// //////// ////////
+// FUNCTIONS
+// set arg DataTypes, then return DataType
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(5, 10));
--- intro/src/index.ts
+++ intro/src/index.ts
@@ -11,6 +11,74 @@
 
 // COMPILE: tsc fileName or tsc --watch fileName
 
-let id: number = 9;
+//////// //////// //////// ////////
 
-console.log(id);
+// BASIC TYPES
+let id: number = 9;
+let company: string = "Omnicorp";
+let isPublihed: boolean = true;
+let x: any = "Hello";
+x = false;
+
+let ids: number[] = [1, 2, 3, 4, 5]; // notation to signify an array of numbers
+// ids.push('Hello') // Would throw an error.
+
+let arr: any[] = [1, "two", true];
+
+//////// //////// //////// ////////
+
+// Tuple
+let person: [number, string, boolean] = [1, "Rob", true]; // datatype must match index position
+
+//////// //////// //////// ////////
+
+// Tuple Array
+let employee: [number, string][];
+employee = [
+  [1, "Rob"],
+  [2, "Burt"],
+  [3, "Gordie"],
+];
+
+// Unions - when you need a variable to hold more than one dataType
+let ex3: string | number = 22;
+
+// enum - define a set up named constants (numeric or string)
+enum Direction1 {
+  Up = 1, //      default value 0, but reset to 1
+  Down, //    default value 1 // now 2
+  Left, //    default value 2 // now 3
+  Right, //   default value 3 // now 4
+}
+console.log(Direction1.Up, Direction1.Right); // should log 0, 3
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+console.log(Direction2.Left); // Retuns "Left"
+
+// Objects - can signify datatypes of key/value object properties
+type User = { id: number; name: string };
+
+const user: User = {
+  id: 1,
+  name: "John",
+};
+
+// Type Assertion - when you want to treat a specific entity as a different type
+let cid: any = 1;
+let customerId = <number>cid;
+// customerId = true; // Will throw an error, because customerId needs to be a number;
+let customerId2 = cid as number;
+
+//////// //////// //////// ////////
+// FUNCTIONS
+// set arg DataTypes, then return DataType
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+console.log(addNum(5, 10));
